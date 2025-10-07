0ffce8d64c00469e010be80d9aceea000295f3ff
Basssiiie <Basssiiie@users.noreply.github.com>
2025-10-07 16:03:36 +0200

    Convert ScEntity to QuickJS (v2) (#7)
    * Convert ScEntity to quickjs
    
    * Fix ScEntity Regiser() and Finalize()
    
    * Convert ScBalloon to quickjs
    
    * Convert ScLitter to quickjs
    
    * Convert ScMoneyEffect to quickjs
    
    * Convert ScParticle to quickjs, replace some method calls with util functions
    
    * Convert ScPeep to quickjs
    
    * Convert ScStaff to quickjs, fix exception return values
    
    * Convert ScGuest to quickjs
    
    * Convert ScVehicle to quickjs, and few other tweaks
    
    * Fix argument numbering in JS unpack macros
    
    * Create and use JS_UNPACK_MONEY64
    
    * Split ScEntity.hpp into ScEntity.cpp, finish entity method overloading code
    
    * Refactor script entity construction code
    
    * Fix merge conflicts
    
    * Ported most of ScMap to quickjs
    
    * Fix getAnimationSprites incorrect arg count, fix build error on size_t
    
    * Add base object functions to Sc objects.
    
    ---------
    
    Co-authored-by: Michael Bernardi <mrmbernardi@gmail.com>
    # Conflicts:
    #	src/openrct2/scripting/bindings/entity/ScEntity.hpp
    #	src/openrct2/scripting/bindings/entity/ScGuest.cpp
    #	src/openrct2/scripting/bindings/entity/ScStaff.cpp
    #	src/openrct2/scripting/bindings/world/ScMap.cpp

--- src/openrct2/scripting/bindings/ride/ScRide.hpp
+++ src/openrct2/scripting/bindings/ride/ScRide.hpp
@@ -9,17 +9,15 @@
 
 #pragma once
 
-#ifdef ENABLE_SCRIPTING
-
-    #include "../../../Context.h"
-    #include "../../../ride/Ride.h"
-    #include "../../Duktape.hpp"
-    #include "../../ScriptEngine.h"
-    #include "../object/ScObject.hpp"
-    #include "ScRideStation.hpp"
+#include "../../../Context.h"
+#include "../../../ride/Ride.h"
+#include "../../ScriptEngine.h"
+#include "../object/ScObject.hpp"
+#include "ScRideStation.hpp"
 
 namespace OpenRCT2::Scripting
 {
+#ifdef ENABLE_SCRIPTING
     template<>
     inline DukValue ToDuk(duk_context* ctx, const TrackColour& value)
     {
@@ -39,29 +37,32 @@ namespace OpenRCT2::Scripting
         result.supports = static_cast<Drawing::Colour>(AsOrDefault(s["supports"], 0));
         return result;
     }
+#endif
 
-    template<>
-    inline DukValue ToDuk(duk_context* ctx, const VehicleColour& value)
+#ifdef ENABLE_SCRIPTING_REFACTOR
+    inline JSValue ToJSValue(JSContext* ctx, const VehicleColour& value)
     {
-        DukObject obj(ctx);
-        obj.Set("body", EnumValue(value.Body));
-        obj.Set("trim", EnumValue(value.Trim));
-        obj.Set("ternary", EnumValue(value.Tertiary));
-        obj.Set("tertiary", EnumValue(value.Tertiary));
-        return obj.Take();
+        JSValue obj = JS_NewObject(ctx);
+        JS_SetPropertyStr(ctx, obj, "body", JS_NewInt32(ctx, EnumValue(value.Body)));
+        JS_SetPropertyStr(ctx, obj, "trim", JS_NewInt32(ctx, EnumValue(value.Trim)));
+        JS_SetPropertyStr(ctx, obj, "ternary", JS_NewInt32(ctx, EnumValue(value.Tertiary)));
+        JS_SetPropertyStr(ctx, obj, "tertiary", JS_NewInt32(ctx, EnumValue(value.Tertiary)));
+        return obj;
     }
 
-    template<>
-    inline VehicleColour FromDuk(const DukValue& s)
+    inline VehicleColour JSToVehicleColours(JSContext* ctx, JSValue val)
     {
         VehicleColour result{};
-        result.Body = static_cast<Drawing::Colour>(AsOrDefault(s["body"], 0));
-        result.Trim = static_cast<Drawing::Colour>(AsOrDefault(s["trim"], 0));
-        result.Tertiary = static_cast<Drawing::Colour>(AsOrDefault(s["ternary"], 0));
-        result.Tertiary = static_cast<Drawing::Colour>(AsOrDefault<int32_t>(s["tertiary"], EnumValue(result.Tertiary)));
+        result.Body = static_cast<Drawing::Colour>(AsOrDefault(ctx, val, "body", 0));
+        result.Trim = static_cast<Drawing::Colour>(AsOrDefault(ctx, val, "trim", 0));
+        result.Tertiary = static_cast<Drawing::Colour>(AsOrDefault(ctx, val, "ternary", 0));
+        result.Tertiary = static_cast<Drawing::Colour>(
+            AsOrDefault(ctx, val, "tertiary", static_cast<uint32_t>(result.Tertiary)));
         return result;
     }
+#endif
 
+#ifdef ENABLE_SCRIPTING
     class ScRide
     {
     private:
@@ -201,6 +202,5 @@ namespace OpenRCT2::Scripting
     public:
         static void Register(duk_context* ctx);
     };
-} // namespace OpenRCT2::Scripting
-
 #endif
+} // namespace OpenRCT2::Scripting
