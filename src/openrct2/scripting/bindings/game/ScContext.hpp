6580cc85b05c8129a39ab5ea862ad881bb9136e5
Basssiiie <Basssiiie@users.noreply.github.com>
2025-10-19 15:21:08 +0200

    Fix crashes from exceptions in getParkStorage and sharedStorage (#13)

--- src/openrct2/scripting/bindings/game/ScContext.hpp
+++ src/openrct2/scripting/bindings/game/ScContext.hpp
@@ -49,7 +49,7 @@ namespace OpenRCT2::Scripting
 
         static JSValue sharedStorage_get(JSContext* ctx, JSValue thisVal)
         {
-            throw std::runtime_error("not implemented."); // this requires support in ScriptEngine.cpp
+            return JS_ThrowPlainError(ctx, "not implemented."); // this requires support in ScriptEngine.cpp
             /* TODO (mber)
             auto& scriptEngine = GetContext()->GetScriptEngine();
             return std::make_shared<ScConfiguration>(ScConfigurationKind::Shared, scriptEngine.GetSharedStorage());
@@ -82,7 +82,7 @@ namespace OpenRCT2::Scripting
 
         static JSValue getParkStorage(JSContext* ctx, JSValue thisVal, int argc, JSValue* argv)
         {
-            throw std::runtime_error("not implemented."); // this requires support in ScriptEngine.cpp
+            return JS_ThrowPlainError(ctx, "not implemented."); // this requires support in ScriptEngine.cpp
             /* TODO (mber) // dukPluginName is argv[0]
             auto& scriptEngine = GetContext()->GetScriptEngine();
 
