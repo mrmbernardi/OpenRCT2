520e08564482384b0b8dd83a15cd250f08af69d1
mrmbernardi <mrmbernardi@gmail.com>
2025-11-22 23:00:32 +1100

    Convert game action script bindings to quickjs (#20)

--- src/openrct2/actions/GameActionRegistry.cpp
+++ src/openrct2/actions/GameActionRegistry.cpp
@@ -213,7 +213,7 @@ namespace OpenRCT2::GameActions
         REGISTER_ACTION(GameSetSpeedAction);
         REGISTER_ACTION(ScenerySetRestrictedAction);
         REGISTER_ACTION(RideSetVisibilityAction);
-#ifdef ENABLE_SCRIPTING
+#ifdef ENABLE_SCRIPTING_REFACTOR
         REGISTER_ACTION(CustomAction);
 #endif
 
